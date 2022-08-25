73d3fd79e7aecddc9cfadcb2a40e096133b3f930
Jim Blandy <jimb@red-bean.com>
2022-08-25 16:53:04 -0700

    Generate tab-separated output, for importing into Google sheets.

--- bin/mergers-and-approvers.sh
+++ bin/mergers-and-approvers.sh
@@ -4,10 +4,14 @@ set -eu
 
 jq --slurpfile pulls pulls.json \
    --slurpfile reviews reviews.json \
+   --raw-output \
    '{
         commit_sha,
         author: .user.login,
-        approvers: [ $reviews[0][.number | tostring].reviews[] | select(.state == "APPROVED") | .user.login ],
+        approvers: [ $reviews[0][.number | tostring].reviews[] | select(.state == "APPROVED") | .user.login] | unique,
         merger: $pulls[0][.number | tostring].merged_by.login
-   }' \
+   }
+   | "\(.commit_sha)\t\(.author)\t\(.approvers | join(","))\t\(.merger)"
+
+' \
    commits.json
