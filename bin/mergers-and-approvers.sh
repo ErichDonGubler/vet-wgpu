e60f66c775a76e79b707dded3be23c9d606442f7
Jim Blandy <jimb@red-bean.com>
2022-08-25 17:39:29 -0700

    Generate "vetted" column.

--- bin/mergers-and-approvers.sh
+++ bin/mergers-and-approvers.sh
@@ -4,6 +4,7 @@ set -eu
 
 jq --slurpfile pulls pulls.json \
    --slurpfile reviews reviews.json \
+   --argjson trusted '{ "jimblandy": true, "kvark": true, "nical": true }' \
    --raw-output \
    '{
         commit_sha,
@@ -11,7 +12,13 @@ jq --slurpfile pulls pulls.json \
         approvers: [ $reviews[0][.number | tostring].reviews[] | select(.state == "APPROVED") | .user.login] | unique,
         merger: $pulls[0][.number | tostring].merged_by.login
    }
-   | "\(.commit_sha)\t\(.author)\t\(.approvers | join(","))\t\(.merger)"
+   | ( .vetted =
+           (.author | in($trusted))
+           or (.merger | in($trusted))
+           or (.approvers | map(in($trusted)) | any)
+     )
+   | ( .status = if .vetted then "vetted" else "needs vet" end )
+   | "\(.commit_sha)\t\(.author)\t\(.approvers | join(","))\t\(.merger)\t\(.status)"
 
 ' \
    commits.json
