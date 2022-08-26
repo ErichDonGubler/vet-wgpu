d2fe78042b1498ae656af419bd8ca86a2b8de0af
Jim Blandy <jimb@red-bean.com>
2022-08-25 18:06:16 -0700

    Do it all in a single jq command.

--- bin/mergers-and-approvers.sh
+++ bin/mergers-and-approvers.sh
@@ -6,19 +6,22 @@ jq --slurpfile pulls pulls.json \
    --slurpfile reviews reviews.json \
    --argjson trusted '{ "jimblandy": true, "kvark": true, "nical": true }' \
    --raw-output \
-   '{
-        commit_sha,
-        author: .user.login,
-        approvers: [ $reviews[0][.number | tostring].reviews[] | select(.state == "APPROVED") | .user.login] | unique,
-        merger: $pulls[0][.number | tostring].merged_by.login
-   }
-   | ( .vetted =
-           (.author | in($trusted))
-           or (.merger | in($trusted))
-           or (.approvers | map(in($trusted)) | any)
-     )
-   | ( .status = if .vetted then "vetted" else "needs vet" end )
-   | "\(.commit_sha)\t\(.author)\t\(.approvers | join(","))\t\(.merger)\t\(.status)"
-
-' \
+   --slurp \
+   'map(
+        {
+            pull: .number,
+            commit_sha,
+            author: .user.login,
+            approvers: [ $reviews[0][.number | tostring].reviews[]
+                         | select(.state == "APPROVED")
+                         | .user.login
+                       ]
+                       | unique,
+            merger: $pulls[0][.number | tostring].merged_by.login
+        }
+        | ( .vetters = ( [ .author, .merger, .approvers[] | select(in($trusted)) ] | unique) )
+   )
+   | sort_by(.pull)[]
+   | "\(.pull)\t\(.commit_sha)\t\(.author)\t\(.approvers | join(","))\t\(.merger)\t\(.vetters | join(","))"
+   ' \
    commits.json
